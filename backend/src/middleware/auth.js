a2e5bba9c36ed6586ac258f652eef534c2ba7f84
iceticshacker7 <iceandcold18@gmail.com>
2024-04-28 20:56:17 +0530

    auth edited 2

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -13,6 +13,7 @@ const auth = async (req, res, next) => {
       console.log("user verified ");
       next();
     } else {
+      const verifyUser = jwt.verify(token, process.env.SECRET_KEY);
       console.log("user not verified");
       res.status(401).send({ data: "user not verified",verifyUser,token });
     }
