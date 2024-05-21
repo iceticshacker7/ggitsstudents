2ad94d19ab589b15701f7a327b2ed49eab4dcbaf
Alpharex121 <arpitkoshta08@gmail.com>
2024-05-21 21:21:45 +0530

    fixed some more ui bugs

--- frontend/src/components/sidecomponents/Admin/Admincreate.jsx
+++ frontend/src/components/sidecomponents/Admin/Admincreate.jsx
@@ -163,7 +163,7 @@ const Admincreate = () => {
                 Confirm Password <span className="text-red-500">*</span>
               </label>
               <input
-                className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5    dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 id="confirmpassword"
                 name="confirmpassword"
                 type="password"
